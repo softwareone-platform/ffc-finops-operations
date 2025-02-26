136c1fe4695a35d93c0049e58732a911f9df8811
Artur Balabanov <arturbalabanov97@gmail.com>
2025-02-26 14:44:00 +0000

    feat: Allow partial updates when using systems PUT endpoint (MPT-7365) (#90)
    * feat: Allow partial updates when using systems PUT endpoint (MPT-7365)
    
    * feat: Add a check and test for empty update (MPT-7365)

--- app/schemas/systems.py
+++ app/schemas/systems.py
@@ -36,8 +36,14 @@ class SystemCreate(SystemBase):
 
 
 class SystemUpdate(BaseSchema):
-    name: str
-    external_id: Annotated[str, Field(max_length=255)]
+    # NOTE: The type annotation of `name` is intentionally `str` and not `str | None`
+    # despite the default value being `None`. This allows partial updatesof fields
+    # other than `name`, however explicitly setting name to `None` fails the validation check.
+    # Also, fields such as `description` are nullable, so we want to differentiate
+    # between an explicit `None` and an unset value.
+
+    name: str = None  # type: ignore[assignment]
+    external_id: Annotated[str | None, Field(max_length=255)] = None
     description: Annotated[str | None, Field(max_length=2000)] = None
     jwt_secret: Annotated[str | None, Field(min_length=64)] = None
 
