a09545ce4e03a672c927bff1233a525ab338a6ba
Artur Balabanov <arturbalabanov97@gmail.com>
2025-05-09 12:00:49 +0100

    Fix the migration to work correctly with existing data (MPT-9810)

--- migrations/versions/0fcca0cf1151_create_datasourceexpense_linked_.py
+++ migrations/versions/0fcca0cf1151_create_datasourceexpense_linked_.py
@@ -20,11 +20,14 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    # Add a new column with "" as the default value (so that the migration can be run on a live database)
-    op.add_column('datasource_expenses', sa.Column('linked_datasource_id', sa.String(length=255), server_default="", nullable=False))
+    # Add the new column with nullable=True as there are existing rows
+    op.add_column('datasource_expenses', sa.Column('linked_datasource_id', sa.String(length=255), nullable=True))
     
-    # Remove the server_default from the column, so that any *future* inserts will require a value to be provided
-    op.alter_column( 'datasource_expenses', 'linked_datasource_id', server_default=None)
+    # Set the default value for existing rows to an empty string
+    op.execute(sa.text("UPDATE datasource_expenses SET linked_datasource_id = ''"))
+    
+    # Remove the nullable constraint now that all existing rows have a value
+    op.alter_column( 'datasource_expenses', 'linked_datasource_id', existing_type=sa.String(length=255), nullable=False)
 
 
 def downgrade() -> None:
