11d3c8b75dc91853d9f658f3908441bd10ef6107
Francesco Faraone <francesco.faraone@softwareone.com>
2025-06-23 08:55:47 +0200

    MPT-11129 improve api spec

--- tests/test_openapi.py
+++ tests/test_openapi.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 
+from app.conf import Settings
 from app.openapi import generate_openapi_spec
 
 
-def test_gen_openapi(fastapi_app: FastAPI):
-    spec = generate_openapi_spec(fastapi_app)
+def test_gen_openapi(fastapi_app: FastAPI, test_settings: Settings):
+    spec = generate_openapi_spec(fastapi_app, test_settings)
     assert "## Available RQL filters" in spec["paths"]["/accounts"]["get"]["description"]
